fdbfd9fafc52d5778937006e8ab18bd914ff7cf6
Narmatsu <joncyy_19@hotmail.com>
2024-04-21 16:33:51 -0500

    Recent Works component was added

---
+++ src/app/components/RecentWorks/RecentWorks.data.js
@@ -0,0 +1,79 @@
+import KromacUIApp from "../../kromac.png";
+import TripCalculatorApp from "../../tripCalculatorApp.png";
+import PokedexApp from "../../pokedexApp.png";
+import CoachBootsApp from "../../coachboostApp.jpg";
+import WordPuzzleApp from "../../wordpuzzleApp.jpg";
+import JtytApp from "../../jtytApp.jpg";
+
+import CoachBootsUIUX from "../../coachbootsUIUX.jpg";
+import ShopAppUIUX from "../../shopUIUX.jpg";
+
+export const recentWorksTitle = "My Recent Works";
+
+export const recentWorksDescription =
+  "We put your ideas and thus your wishes in the form of a unique web project that inspires you and you customers.";
+
+export const filterOptions = ["All", "Apps", "UI/UX"];
+
+export const projectList = [
+  {
+    img: KromacUIApp,
+    name: "Kromac-UI",
+    type: "Apps",
+    link: "https://www.kromac-ui.com/",
+    description:
+      "100% responsive style framework that seeks to facilitate the development.",
+  },
+  {
+    img: CoachBootsApp,
+    name: "Coach Boots (in progress)",
+    type: "Apps",
+    link: "https://lolhugrock-deploy.vercel.app/",
+    description:
+      "League of Legends app for boosting or find coaching by profesionals.",
+  },
+  {
+    img: PokedexApp,
+    name: "Pokedex",
+    type: "Apps",
+    link: "https://ciandt-tech-challenge-ten.vercel.app/",
+    description: "Pokedex to search, fav and fight.",
+  },
+  {
+    img: TripCalculatorApp,
+    name: "Trip Calculator",
+    type: "Apps",
+    link: "https://challenge-jonathan-narvaez.vercel.app/",
+    description: "Simple App to calculate the distance between several points.",
+  },
+  {
+    img: WordPuzzleApp,
+    name: "Word Puzzle",
+    type: "Apps",
+    link: "https://peacku-challenge-jonathan-narvaez.vercel.app/",
+    description: "Simple word puzzle to enjoin a little.",
+  },
+  {
+    img: JtytApp,
+    name: "Jowy Travels & Tours (in progress)",
+    type: "Apps",
+    link: "https://jowy-travel-tours-deploy.vercel.app/",
+    description:
+      "Turistic landingpage for turistic agency in Cartagena Colombia.",
+  },
+  {
+    img: CoachBootsUIUX,
+    name: "Coach Boots (in progress)",
+    type: "UI/UX",
+    link: "https://www.figma.com/file/NBjXfncA7SYVflILQt7KgJ/LolHugrock?type=design&node-id=0%3A1&mode=design&t=L7xgFt938k0pgjWJ-1",
+    description:
+      "Coach boots UI/UX design: League of Legends app for boosting or find coaching by profesionals.",
+  },
+  {
+    img: ShopAppUIUX,
+    name: "Shop App (In progress)",
+    type: "UI/UX",
+    link: "https://www.figma.com/file/2TKzvj2VWzVtMwrEpUfEJb/Untitled?type=design&node-id=0-1&mode=design&t=9xr19XqwJ0Ltg8Uw-0",
+    description: "Shop App UI/UX design.",
+  },
+];
---
+++ src/app/components/RecentWorks/RecentWorks.jsx
@@ -0,0 +1,61 @@
+"use client";
+import React, { useState } from "react";
+import styles from "./RecentWorks.module.scss";
+import Image from "next/image";
+import Link from "next/link";
+import {
+  filterOptions,
+  projectList,
+  recentWorksDescription,
+  recentWorksTitle,
+} from "./RecentWorks.data";
+
+const RecentWorks = () => {
+  const [activeTab, setActiveTab] = useState(filterOptions.at(0));
+  const [projectsFiltered, setProjectsFiltered] = useState(projectList);
+  const [selectorLeft, setSelectorLeft] = useState(0);
+
+  const isActiveTab = (tab) => (activeTab === tab ? styles.active : styles.p);
+
+  const handlerFilter = (tab) => {
+    setActiveTab(tab);
+    setSelectorLeft(filterOptions.findIndex((option) => option === tab));
+    setProjectsFiltered(
+      tab === filterOptions.at(0)
+        ? projectList
+        : projectList.filter((projects) => projects.type === tab)
+    );
+  };
+
+  return (
+    <section className={styles.recentWorks}>
+      <h2>{recentWorksTitle}</h2>
+      <p>{recentWorksDescription}</p>
+      <section className={styles.filter}>
+        {filterOptions.map((option, index) => (
+          <p
+            key={index}
+            onClick={() => handlerFilter(option)}
+            className={isActiveTab(option)}
+          >
+            {option}
+          </p>
+        ))}
+        <p style={{ "--i": selectorLeft }} className={styles.selector} />
+      </section>
+      <section className={styles.projectsList}>
+        {projectsFiltered.map((project, index) => (
+          <section key={index} className={styles.projectContainer}>
+            <Image src={project.img} alt={project.name} />
+            <Link href={project.link} target="__blank">
+              <h3>{project.name}</h3>
+              <p>{project.description}</p>
+            </Link>
+          </section>
+        ))}
+      </section>
+    </section>
+  );
+};
+
+export default RecentWorks;
