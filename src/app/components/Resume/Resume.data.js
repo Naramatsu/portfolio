595f24306213e1e6d281c41015123c0c202ed44d
Narmatsu <joncyy_19@hotmail.com>
2024-04-23 09:01:19 -0500

    added more info to Resumen component

--- src/app/components/Resume/Resume.data.js
+++ src/app/components/Resume/Resume.data.js
@@ -4,26 +4,162 @@ export const experienceList = [
     period: "2021 - Cuerrent",
     title: "Frontend Developer",
     company: "Freelance",
+    info: {
+      description: (
+        <p>
+          Kromac-UI (Style framework) — is a 100% responsive style framework
+          that seeks to facilitate the development and implementation of
+          eye-catching components with modern styles to web applications.
+        </p>
+      ),
+      techs: [
+        "HTML5",
+        "CSS3",
+        "SASS",
+        "JavaScript",
+        "ReactJs",
+        "React hooks",
+        "ES6",
+        "Git",
+        "Github",
+        "Webpack",
+        "Figma",
+        "Jest",
+      ],
+    },
   },
   {
     period: "2023 - 2024",
     title: "Frontend Developer",
     company: "Tech Mahindra",
+    info: {
+      description: (
+        <p>
+          Colpatria bank application — Creation of an application in ReactJS +
+          Redux and others, conformed by a group of 2 QA testers, 2 business
+          analysts, 1 agile facilitator, 4 backend developers and 4 frontend
+          developers, each one was in charge of features created in the backlog
+          according to the Product Owner projections.
+        </p>
+      ),
+      techs: [
+        "HTML5",
+        "CSS3",
+        "Javascript",
+        "ReactJs",
+        "React hooks",
+        "ES6",
+        "Git",
+        "Bitbucket",
+        "Redux",
+        "Redux-thunk",
+        "styled-components",
+        "Jira",
+        "Jest",
+        "Api REST",
+        "Mockups",
+      ],
+    },
   },
   {
     period: "2022 - 2022",
     title: "Frontend Developer",
     company: "EPAM",
+    info: {
+      description: (
+        <p>
+          Growth (Improving and acquiring knowledge) — Reinforcing knowledge in
+          ReactJS, React hooks, ReactJs + Typescript, React Native (for android)
+          and NextJS. Learning basic concepts in user interface design with
+          Figma, I worked on personal projects like: OAuth login services with
+          JWT + Google + NodeJS, News application for android devices, Mockups
+          for a Store application.
+        </p>
+      ),
+      techs: [
+        "HTML5",
+        "CSS3",
+        "Javascript",
+        "ReactJs",
+        "React hooks",
+        "SASS",
+        "ES6",
+        "Git",
+        "Github",
+        "Typescript",
+        "React Native",
+        "NextJs",
+        "Figma",
+        "Aws",
+        "Google",
+        "NodeJs",
+      ],
+    },
   },
   {
     period: "2020 - 2022",
     title: "Fullstack Developer",
     company: "Condor labs",
+    info: {
+      description: (
+        <p>
+          Education web application — Creation of an application in ReactJS +
+          SASS + Redux + NojdeJS + Express + MongoDB + OracleDB + AWS stack,
+          conformed by a group of 3 backend developers and 1 frontend developer,
+          each one was in charge of features created in the backlog according to
+          the Product Owner projections.
+        </p>
+      ),
+      techs: [
+        "HTML5",
+        "CSS3",
+        "Javascript",
+        "ReactJs",
+        "React hooks",
+        "Bootstrap",
+        "Emerald-UI",
+        "SASS",
+        "ES6",
+        "Redux",
+        "Redux-thunk",
+        "NodeJs",
+        "Express",
+        "AWS",
+        "MongoDB",
+        "Oracle",
+        "Redis",
+        "Git",
+        "Github",
+        "Jest",
+        "Api REST",
+        "Mockups",
+      ],
+    },
   },
   {
     period: "2019 - 2020",
     title: "Fullstack Developer",
     company: "Zeus",
+    info: {
+      description: (
+        <p>
+          Restaurant Application — Maintaining an application in Foxpro, Visual
+          Basic and migrating to ASP .Net, conformed by a group of 3 full stack
+          developers, each one was in charge of features created in the backlog
+          according to the Product Owner projections.
+        </p>
+      ),
+      techs: [
+        "HTML5",
+        "CSS3",
+        "Javascript",
+        "SQL Server",
+        "C#",
+        "ASP .NET",
+        "Visual Basic",
+        "Fox Pro",
+      ],
+    },
   },
 ];
 
@@ -38,5 +174,41 @@ export const educationList = [
     period: "2013",
     title: "High school",
     company: "Institucion Educativa Docente de Turbaco",
+  },
+];
+
+export const resumeCertificationsTitle = "LinkedIn Certifications";
+export const certificationList = [
+  {
+    title: "Software Design Patterns",
+    link: "#",
+  },
+  {
+    title: "Redux",
+    link: "#",
+  },
+  {
+    title: "NextJS",
+    link: "#",
+  },
+  {
+    title: "TypeScript",
+    link: "#",
+  },
+  {
+    title: "React Native",
+    link: "#",
+  },
+  {
+    title: "GraphQL",
+    link: "#",
+  },
+  {
+    title: "User Experience Design",
+    link: "#",
+  },
+  {
+    title: "Figma",
+    link: "#",
   },
 ];
--- src/app/components/Resume/Resume.jsx
+++ src/app/components/Resume/Resume.jsx
@@ -1,22 +1,41 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 
 import styles from "./Resume.module.scss";
 import {
+  certificationList,
   educationList,
   experienceList,
+  resumeCertificationsTitle,
   resumeEducationTitle,
   resumeExperienceTitle,
 } from "./Resume.data";
+import ModalExperience from "../ModalExperience";
 
 const Resume = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [experienceSelected, setExperienceSelected] = useState(null);
+
+  const handlerShowModal = (info) => {
+    if (showModal) return setShowModal(false);
+    setExperienceSelected(info);
+    setShowModal(true);
+  };
+
   return (
     <section className={styles.resume}>
       <section className={styles.resumeContainer}>
         <section className={styles.resumeOverview}>
           <h2>{resumeExperienceTitle}</h2>
           <section className={styles.resumeItemList}>
-            {experienceList.map(({ period, title, company }, index) => (
-              <section key={index} className={styles.resumeItem}>
+            {experienceList.map(({ period, title, company, info }, index) => (
+              <section
+                key={index}
+                className={styles.resumeItem}
+                onClick={() =>
+                  handlerShowModal({ ...info, period, title, company })
+                }
+              >
                 <label>{period}</label>
                 <h3>{title}</h3>
                 <p>{company}</p>
@@ -35,8 +54,23 @@ const Resume = () => {
               </section>
             ))}
           </section>
+          <h2 style={{ marginTop: "50px" }}>{resumeCertificationsTitle}</h2>
+          <section className={styles.resumeCertificationList}>
+            {certificationList.map(({ title, link }, index) => (
+              <section key={index} className={styles.certificationItems}>
+                <a href={link} target="__blank" rel="noreferrer">
+                  {title}
+                </a>
+              </section>
+            ))}
+          </section>
         </section>
       </section>
+      <ModalExperience
+        isVisible={showModal}
+        info={experienceSelected}
+        onClose={() => setShowModal(false)}
+      />
     </section>
   );
 };
