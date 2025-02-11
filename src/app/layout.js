42247786782101a1164423e9902d7faddfbe8794
Naramatsu <joncyy_19@hotmail.com>
2025-02-11 14:34:18 -0500

    added some metadatas

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,6 +7,21 @@ export const metadata = {
   title: "Jonathan Narvaez Martinez - Portfolio",
   description:
     "Software engineer, Frontend Developer, React JS Developer portfolio",
+  openGraph: {
+    title: "Jonathan Narvaez Martinez - Portfolio",
+    description:
+      "Software engineer, Frontend Developer, React JS Developer portfolio",
+    images: [
+      {
+        url: "https://curriculum-jcnm.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fprofile.33fe3f39.jpg&w=3840&q=75",
+        width: 466,
+        height: 630,
+        alt: "Jonathan Narvaez Martinez",
+      },
+    ],
+    url: "https://curriculum-jcnm.vercel.app/",
+    type: "website",
+  },
 };
 
 export default function RootLayout({ children }) {
